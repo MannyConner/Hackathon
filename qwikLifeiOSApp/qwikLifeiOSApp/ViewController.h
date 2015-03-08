8ab6c32df9b669c4cd3b47c5d7d0c39d68dcf5e6
Steffen Frost <github@steffenfrost.com>
2015-03-08 11:22:04 -0700

    Adding Xcode starter project
    Start Xcode

---
+++ qwikLifeiOSApp/qwikLifeiOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qwikLifeiOSApp
+//
+//  Created by Steven Frost-Ruebling on 3/8/15.
+//  Copyright (c) 2015 Carticipate, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
